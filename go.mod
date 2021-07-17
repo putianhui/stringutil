e3a4d72b79e7a8a3c51470dba50595ec9a6c03ad
putianhui <putianhui@putianhuideMacBook-Pro.local>
2021-07-17 15:01:10 +0800

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/putianhui/stringutil
+
+go 1.16
---
+++ rand.go
@@ -0,0 +1,11 @@
+/**
+ * @Author ❤PuTianHui❤
+ * @Date 2021/7/17-2:58 下午
+ **/
+package stringutil
+
+import "fmt"
+
+func RandString()  {
+	fmt.Println("xiaopu")
+}
